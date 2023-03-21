8f99ed12b5c26775715273f0ab49a01ad712e0c0
wilsonibekason <wanbekgithub@gmail.com>
2023-03-21 04:05:18 -0700

    Add tictactoe UI

--- src/components/home/HomeLayout.tsx
+++ src/components/home/HomeLayout.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AiOutlinePlus, BiPlus, AiFillCiCircle } from "react-icons/all";
 interface SquaresProp {
   value: string;
   onSquareClick(): void;
@@ -9,7 +10,14 @@ interface BoardProps {
   onPlay(nextSquare: any[]): void;
 }
 const Squares: React.FC<SquaresProp> = ({ value, onSquareClick }) => {
-  return <button onClick={onSquareClick}>{value}</button>;
+  return (
+    <button
+      onClick={onSquareClick}
+      className="w-12 h-12 border-2 border-gray-100 flex justify-center items-center"
+    >
+      {value}
+    </button>
+  );
 };
 const Boards: React.FC<BoardProps> = ({ xIsNext, squares, onPlay }) => {
   const handleClick = (i: number) => {
@@ -28,21 +36,33 @@ const Boards: React.FC<BoardProps> = ({ xIsNext, squares, onPlay }) => {
       {/* status */}
       <div>{status}</div>
       {/* Board Row */}
-      <div className="board-row">
-        <Squares value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Squares value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Squares value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <div className="my-10 grid grid-cols-3 w-80 gap-y-4">
+        {squares.map((square, i) => (
+          <Squares
+            key={i}
+            value={square}
+            onSquareClick={() => handleClick(i)}
+          />
+        ))}
       </div>
-      <div className="board-row">
-        <Squares value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Squares value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Squares value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Squares value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Squares value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Squares value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {/* <div className="flex flex-col space-y-5">
+        <div className="board-row ">
+          <Squares value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Squares value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Squares value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Squares value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Squares value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Squares value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Squares value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Squares value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Squares value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </div> */}
+
       {/* Board Row */}
     </>
   );
@@ -65,12 +85,17 @@ const HomeLayout = () => {
 
   const moves = history.map((squares, move) => {
     let description: string;
-    if (move > 0) description = "Go to move *" + move;
+    if (move > 0) description = "Go to move * " + move;
+    // else if (move < 2) description = "CLEAR moves history";
     else description = "Go to game start";
     return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
+      <>
+        <li key={move} className="">
+          <button onClick={() => jumpTo(move)} className="text-sm">
+            {description}
+          </button>
+        </li>
+      </>
     );
   });
   return (
@@ -83,8 +108,21 @@ const HomeLayout = () => {
         />
       </div>
       <div>
-        <h1>Moves Taken</h1>
-        <ol>{moves}</ol>
+        <h1 className="text-sm font-mono tracking-wide font-semibold my-5">
+          Moves Taken
+        </h1>
+        {/* <div className="cube">
+          <div className="top"></div>
+          <div>
+            <span id="--i:0;">youtube.com/programmerRimon</span>
+            <span id="--i:1;">youtube.com/programmerRimon</span>
+            <span id="--i:2;">youtube.com/programmerRimon</span>
+            <span id="--i:3;">youtube.com/programmerRimon</span>
+          </div>
+        </div> */}
+        <ul className="grid grid-cols-4 gap-4 p-5 bg-slate-600 font-roman">
+          {moves}
+        </ul>
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roman": ["Varius1LTW01-Roman", "cursive"]
+      }
+    },
   },
   plugins: [],
 };
